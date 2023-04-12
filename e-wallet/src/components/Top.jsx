9a977438bcf4bb909b5a012800afa50e7ec64718
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-04-12 17:50:31 +0200

    Css for homepage done

--- e-wallet/src/components/Top.jsx
+++ e-wallet/src/components/Top.jsx
@@ -1,9 +1,9 @@
-
+import './Top.scss'
 
 export default function Top() {
 
   return (
-    <header>
+    <header className='top'>
         <h1>E-WALLET</h1>
     </header>
   )
--- e-wallet/src/views/Home.jsx
+++ e-wallet/src/views/Home.jsx
@@ -6,15 +6,16 @@ import './Home.scss'
 export default function Home() {
 
   return (
-    <section>
+    <section className='Home'>
         <Top />
         <main>
-            <p>ACTIVE CARD</p>
+            <p className='HomeActive'>ACTIVE CARD</p>
             <div className='mainContent'>
                 <Card />
                 <CardStack />
             </div>
         </main>
+        <button>ADD A NEW CARD</button>
     </section>
   )
 }
